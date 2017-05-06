6cf9cc7b699c6c7cd8a9e0a33c304deddb38b76b
Daniel <danieldanieltalortalor@gmail.com>
2017-05-06 23:53:03 +0300

    Change db uri
    Signed-off-by: Daniel <danieldanieltalortalor@gmail.com>

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ var handlebars = require('express-handlebars');
 // Connect to mongoDB 
 var mongoose = require('mongoose');
 //mongoose.connect('mongodb://localhost/StorageApplicationDB');
-mongoose.connect('mongodb://danieltalor:DanielTalor@ds131061.mlab.com:31061/storage-application-db');
+mongoose.connect('mongodb://danieltalor:DanielTalor@ds133231.mlab.com:33231/storage-application-db');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
